ae1fc061bd6facca8ddba69cc90a4da718a4e2a5
Zachary <zbberman@yahoo.com>
2019-01-30 15:07:47 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -19,11 +19,11 @@ def last_element(array)
 end
 
 def first_element_with_array_methods(array)
-  array.shift()
+  array.first
 end
 
 def last_element_with_array_methods(array)
-  array.pop()
+  array.last
 end
 
 def length_of_array(array)
